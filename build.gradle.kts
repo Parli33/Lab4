2f58ca3a71275bde2774f97cbda320bcf4c0f6dd
Parli33 <theili9a@gmail.com>
2024-11-02 20:57:58 +0700

    update gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,16 +10,18 @@ repositories {
 }
 
 dependencies {
-    compileOnly ("org.projectlombok:lombok:1.18.22")
-    annotationProcessor ("org.projectlombok:lombok:1.18.22")
-    testCompileOnly ("org.projectlombok:lombok:1.18.22")
-    testAnnotationProcessor ("org.projectlombok:lombok:1.18.22")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-    implementation ("com.google.code.gson:gson:2.8.5")
+    implementation ("org.projectlombok:lombok:1.18.30")
+    annotationProcessor ("org.projectlombok:lombok:1.18.30")
+    testImplementation ("org.projectlombok:lombok:1.18.30")
+    testAnnotationProcessor ("org.projectlombok:lombok:1.18.30")
+    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    implementation ("com.google.code.gson:gson:2.10.1")
 }
 
 
-tasks.getByName<Test>("test") {
+
+
+tasks.test {
     useJUnitPlatform()
 }
